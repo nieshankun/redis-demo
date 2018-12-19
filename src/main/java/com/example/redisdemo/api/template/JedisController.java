8bdd1a13ffcd2e95de7874b63948015013311820
nieshankun <38554448+nieshankun@users.noreply.github.com>
2018-12-19 09:38:19 +0800

    saving image to redis（cluster）

--- src/main/java/com/example/redisdemo/api/template/JedisController.java
+++ src/main/java/com/example/redisdemo/api/template/JedisController.java
@@ -1,12 +1,24 @@
 package com.example.redisdemo.api.template;
 
+import com.example.redisdemo.api.utils.Base64ImageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.mail.javamail.ConfigurableMimeFileTypeMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import redis.clients.jedis.JedisCluster;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author nsk
@@ -15,18 +27,67 @@ import redis.clients.jedis.JedisCluster;
 @RestController
 @RequestMapping("/jedis")
 public class JedisController {
+
+    private final ConfigurableMimeFileTypeMap configurableMimeFileTypeMap = new ConfigurableMimeFileTypeMap();
+
     @Autowired
     private JedisCluster jedisCluster;
 
     @GetMapping("/set-string")
-    public ResponseEntity setString(){
-        long result = jedisCluster.setnx("height","168");
+    public ResponseEntity setString() {
+        long result = jedisCluster.setnx("weight", "62");
         return ResponseEntity.ok(result);
     }
 
     @GetMapping("/{key}")
-    public ResponseEntity getValueByKey(@PathVariable String key){
+    public ResponseEntity getValueByKey(@PathVariable String key) {
         String name = jedisCluster.get(key);
         return ResponseEntity.ok(name);
     }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public ResponseEntity uploadImage(@RequestParam("file") MultipartFile file) {
+        String id = UUID.randomUUID().toString();
+        String content = Base64ImageUtils.encodeImageToBase64(file);
+        Map<String, String> criteria = new HashMap<>();
+        criteria.put("content", content);
+        criteria.put("fileName", file.getOriginalFilename());
+        criteria.put("size", String.valueOf(file.getSize()));
+        jedisCluster.hmset(id, criteria);
+        Map<String, String> map = new HashMap<>();
+        map.put("id", id);
+        return ResponseEntity.ok(map);
+    }
+
+    @RequestMapping(path = "/download/{id}", method = RequestMethod.GET)
+    public ResponseEntity<InputStreamResource> downloadResource(@PathVariable String id) {
+        Map<String, String> criteria = jedisCluster.hgetAll(id);
+        return ResponseEntity.ok()
+                .headers(httpHeaders(criteria.get("size"), criteria.get("fileName")))
+                .cacheControl(CacheControl.maxAge(Long.MAX_VALUE, TimeUnit.HOURS).cachePublic())
+                .body(body(criteria.get("content")));
+    }
+
+
+    private InputStreamResource body(String content) {
+        return new InputStreamResource(Base64ImageUtils.decodeBase64ToImage(content));
+    }
+
+    private HttpHeaders httpHeaders(String size, String name) {
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.valueOf(configurableMimeFileTypeMap.getContentType(name)));
+        httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename*=UTF-8''" + encode(name));
+        httpHeaders.set(HttpHeaders.CONTENT_LENGTH, size);
+        return httpHeaders;
+    }
+
+    private String encode(String name) {
+        try {
+            return URLEncoder.encode(name, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+
 }
---
+++ src/main/java/com/example/redisdemo/api/utils/Base64ImageUtils.java
@@ -0,0 +1,81 @@
+package com.example.redisdemo.api.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.MalformedURLException;
+
+/**
+ * @author nsk
+ * 2018/12/19 8:19
+ */
+public class Base64ImageUtils {
+
+    private Base64ImageUtils() {
+        // for sonar
+    }
+
+    public static String encodeImageToBase64(MultipartFile file) {
+        byte[] bytes = null;
+        try {
+            bytes = file.getBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(bytes).trim();
+    }
+
+    public static InputStream decodeBase64ToImage(String base) {
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] decodeBytes = null;
+        try {
+            decodeBytes = decoder.decodeBuffer(base);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ByteArrayInputStream(decodeBytes);
+    }
+
+    public static String encodeImageToBase64(File file) {
+        ByteArrayOutputStream outputStream = null;
+        try {
+            BufferedImage bufferedImage = ImageIO.read(file);
+            outputStream = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, "png", outputStream);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException ee) {
+            ee.printStackTrace();
+        }
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(outputStream.toByteArray()).trim();
+    }
+
+    public static void decodeBase64ToImage(String base,String path,String imageName){
+        BASE64Decoder decoder = new BASE64Decoder();
+        FileOutputStream write = null;
+        try{
+            write = new FileOutputStream(new File(path+imageName));
+            byte[] decodeBytes = decoder.decodeBuffer(base);
+            write.write(decodeBytes);
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if (write != null){
+                    write.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+}
